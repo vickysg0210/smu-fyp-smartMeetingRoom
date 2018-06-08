b62afcf449ccc3e95fd26b6776fdd0f854ceb84a
zifupeng <zifu.peng.2015@sis.smu.edu.sg>
2018-06-08 16:42:38 +0800

    added toolbar to home page and attendees page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  public pageName : String = "home";
-  
+  public pageName: String = 'home';
   constructor() { }
 
   ngOnInit() {
