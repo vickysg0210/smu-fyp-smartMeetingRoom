017e27070d93d4830fbfcf0c528da0ffb3932d34
vickysg0210 <vickysg0210@gmail.com>
2018-09-13 12:51:54 +0800

    update

--- src/app/components/analysis-heatmap/analysis-heatmap.component.ts
+++ src/app/components/analysis-heatmap/analysis-heatmap.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ElementRef, OnInit } from '@angular/core';
+
+declare let h337: any;
 
 @Component({
   selector: 'app-analysis-heatmap',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalysisHeatmapComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private elRef: ElementRef) { }
 
   ngOnInit() {
+    this.heatmap();
   }
 
+  heatmap(){
+    // minimal heatmap instance configuration
+    var heatmapInstance = h337.create({
+      // only container is required, the rest will be defaults
+      container: document.querySelector('.heatmap')
+    });
+
+    // now generate some random data
+    var points = [];
+    var max = 0;
+    var width = 120;
+    var height = 180;
+    var len = 50;
+
+    while (len--) {
+      var val = Math.floor(Math.random()*100);
+      max = Math.max(max, val);
+      var point = {
+        // x: 60,
+        // y: 90,
+        x: Math.floor(Math.random()*width),
+        y: Math.floor(Math.random()*height),
+        value: val
+      };
+      points.push(point);
+    }
+    // heatmap data format
+    var data = {
+      max: max,
+      data: points
+    };
+    // if you have a set of datapoints always use setData instead of addData
+    // for data initialization
+    heatmapInstance.setData(data);
+
+  }
 }
